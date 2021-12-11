f8962ac9ba23b6967e065e86333b6f6d9fe41e6c
juwiragiye <uwiragiyejanvi@gmail.com>
2021-12-11 15:45:42 -0500

    generated

---
+++ app/root.tsx
@@ -0,0 +1,16 @@
+import { LiveReload } from "remix";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <title>Jokes Remix App</title>
+      </head>
+      <body>
+        Hello world
+        {process.env.NODE_ENV === "development" ? <LiveReload /> : null}
+      </body>
+    </html>
+  );
+}
